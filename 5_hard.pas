b8c4417969aae0e617d3fd29d4eb92132b962bff
Robocatt <48563117+Robocatt@users.noreply.github.com>
2021-03-22 00:04:42 +0300

    21.02.21

--- 5_hard.pas
+++ 5_hard.pas
@@ -1,57 +1,53 @@
-﻿var r,ans,a,i:Integer;
-s,g: string; 
-ch:char;
-f:boolean;
-begin 
-  writeln('Введите строку'); 
-  readln(s);
-  f:=true;
-  while length(s) <>0 do 
-   begin 
-   if f then
-    begin 
-    for i:=1 to length(s) do 
-     begin
-      if not (s[i]  in ['0'..'9']) then 
-        begin
-        g:= copy(s,1,i-1);
-        val(g,a,r);
-        ans:=a;
-        delete(s,1,i-1);
-        f:=false;
-        break;
-        end;
-     end;
-    end
+﻿var ans,a,i,r:Integer;
+s,s2: string; 
+symbol:char;
+flag:boolean;
+begin
+flag:=true; 
+writeln('Введите выражение в строчку'); 
+readln(s);  
+  while length(s) <> 0 do begin 
+  if flag then begin 
+  for i:=1 to length(s) do begin
+   if not (s[i]  in ['0'..'9']) then begin
+    s2:= copy(s,1,i-1);
+    val(s2,a,r);
+    ans:=a;
+     delete(s,1,i-1);
+   flag:=false;
+  break;
+end;
+end;
+end
     else 
     begin
-     ch:=s[1];
+     symbol:=s[1];
      delete(s,1,1);
      for i:=1 to length(s) do 
      begin
-      if not (s[i]  in ['0'..'9']) then 
-       begin
-       g:= copy(s,1,i-1);
-       val(g,a,r); 
-        if ch = '+' then 
-         ans:= ans + a 
-        else 
-         ans:= ans -a; 
-        delete(s,1,i-1);
-        break;
-        end;
-       if i = length(s) then 
-        begin
-         g:= copy(s,1,length(s));
-         val(g,a,r); 
-         if ch = '+' then 
-          ans:= ans + a 
-         else 
-          ans:= ans -a;
-          delete(s,1,i);
-          end;
-      end;
-     end;
-    end;
+   if not (s[i]  in ['0'..'9']) then 
+   begin
+   s2:= copy(s,1,i-1);
+   val(s2,a,r); 
+ if symbol = '+' then 
+  ans:= ans + a 
+  else 
+  ans:= ans -a; 
+   delete(s,1,i-1);
+  break;
+  end;
+  if i = length(s) then 
+  begin
+  s2:= copy(s,1,length(s));
+  val(s2,a,r); 
+  if symbol = '+' then 
+  ans:= ans + a 
+  else 
+  ans:= ans -a;
+  delete(s,1,i);
+end;
+end;
+end;
+end;
   writeln(ans);
 end.
