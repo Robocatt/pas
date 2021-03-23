b8b2f6ff12c6e4b9624d519b331d59e8329ce057
Robocatt <48563117+Robocatt@users.noreply.github.com>
2021-03-23 10:23:14 +0300

    Classwork

---
+++ Новая папка/3.pas
@@ -0,0 +1,32 @@
+﻿
+var c:char;
+z,i,n,goal:integer;
+s,g:string;
+ans: array[1..5] of string;
+begin
+  readln(n);
+  for i:=1 to n do begin
+    s:='';
+    for z:=1 to 2 do begin 
+   repeat read(c);
+   s:=s+c;
+   until c= ' ';
+  end; 
+
+   repeat read(c);
+   until c = ' ';
+  
+  readln(g);
+  val(g,goal,z);
+  
+  if goal > 2 then 
+  ans[i]:= s;
+  end;
+  
+  for i:=1 to 5 do 
+    if ans[i]<>'' then 
+      writeln(ans[i]);
+  
+  
+  
+end.
---
+++ Новая папка/4.pas
@@ -0,0 +1,33 @@
+﻿var c:char;
+z,i,n,goal:integer;
+s,g:string;
+name: array[1..5] of string;
+number: array[1..5] of integer;
+begin
+  readln(n);
+
+  for i:=1 to n do begin
+    s:='';
+    for z:=1 to 2 do begin 
+   repeat read(c);
+   s:=s+c;
+   until c= ' ';
+  end; 
+   repeat read(c);
+   until c = ' ';
+  readln(g);
+  val(g,goal,z);
+  
+  name[i]:=s;
+  number[i]:=goal;
+  end;
+  z:=1;
+  for i:=1 to 5 do begin 
+    if number[i] > z then 
+      z:= i;
+  end;
+  
+  writeln(name[z] + number[z]);
+  
+  
+end.
