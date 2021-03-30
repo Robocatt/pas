04dfef6548990717d59e2521f051f8c8cf0f4ac8
Robocatt <48563117+Robocatt@users.noreply.github.com>
2021-03-30 17:14:24 +0300

    30.03.21 class

---
+++ 3_30_cl.pas
@@ -0,0 +1,17 @@
+﻿var i,s:integer;
+f:text;
+begin
+  assign(f,'data.txt');
+  reset(f);
+  while not eof(f) do begin 
+  readln(f,i);
+  if i mod 2 = 0 then 
+    s:=s+i;
+    i:=0;
+  end;
+  close(f);
+  assign(f,'output_3.txt');
+  rewrite(f);
+  writeln(f,'Сумма четных чисел = ', s);
+  close(f);
+end.
---
+++ 5_30_cl.pas
@@ -0,0 +1,34 @@
+﻿var c,i,temp,cou:integer;
+f:text;
+flag:boolean;
+begin
+  flag:=true;
+  assign(f,'data_5.txt');
+  reset(f);
+  while not eof(f) do begin 
+  readln(f,i);
+    if flag then begin 
+    temp:= i;
+    cou:=1;
+    flag:=false;
+    end
+  else begin     
+  if i = temp then 
+    cou:=cou+1
+  else begin
+    if cou > c then 
+    c:=cou;
+    
+    temp:=i;
+    cou:=1;
+  end;
+  end;
+  end;
+  
+  
+  close(f);
+  assign(f,'output_5.txt');
+  rewrite(f);
+  writeln(f,'Длинна самой длинной цепочки ', c);
+  close(f);
+end.
