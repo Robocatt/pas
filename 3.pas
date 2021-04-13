094d0862444cef0a59133ac49f8fa8d255b74bae
Robocatt <48563117+Robocatt@users.noreply.github.com>
2021-04-13 19:17:12 +0300

    13.04 cl

---
+++ 3.pas
@@ -0,0 +1,32 @@
+﻿var i,j,temp: Integer;
+f: text;
+a:array of integer;
+begin
+  assign(f,'input.txt');
+  reset(f);
+  i:=0;
+  setlength(a,100); 
+  while ((not eof(f)) and (i < 100)) do begin 
+    readln(f,a[i]);
+    inc(i);
+  end;
+  for i:= 1 to length(a) do 
+    if a[i] = 0 then 
+      break;
+  setlength(a,i);
+  for j:=0 to length(a)-1 do begin
+    for i:=0 to length(a)-2 do begin
+      if a[i] < a[i+1] then begin
+        temp:= a[i];
+        a[i]:=a[i+1];
+        a[i+1]:=temp;
+      end;
+   end;
+    end;
+  close(f);
+  assign(f,'output_3.txt');
+  rewrite(f);
+  for i:=0 to length(a)-1 do 
+    writeln(f,a[i]);
+   close(f);
+end.
